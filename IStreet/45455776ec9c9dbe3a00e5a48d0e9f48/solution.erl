6b4e5240fc98db51d3b784940bd13aabb4e0dc65
Greg <Gregt87@gmail.com>
2012-06-06 22:21:57 -0700

    Edited solution in erlang

--- IStreet/45455776ec9c9dbe3a00e5a48d0e9f48/solution.erl
+++ IStreet/45455776ec9c9dbe3a00e5a48d0e9f48/solution.erl
@@ -9,13 +9,29 @@ map_n_times(F, 0) -> [];
 map_n_times(F, Times) -> [F() | map_n_times(F, Times-1)].
  
 get_test_cases(NumCases) ->
-  map_n_times(fun() -> solve_test_case(get_case(get_int_from_stdin())) end, NumCases).
+  map_n_times(fun() -> win_test_case(get_case(get_int_from_stdin())) end, NumCases).
 
 get_case(NumElements) ->
   map_n_times(fun() -> get_int_from_stdin() end, NumElements).
 
-solve_test_case(TestCase) ->
-  TestCase.
+is_sorted(Array) ->
+  case list:sort(Array) == Array of
+    true -> true;
+    false -> false
+  end.
+  
+
+win_test_case(TestCase) ->
+  NextCases = [list:delete(Element, TestCase) || Element <- TestCase],
+
+  case list:any(fun(Element) -> is_sorted(Element) end, NextCases) of
+    true -> true;
+    false -> case list:all(fun(Element) -> win_test_case(Element) == false end, NextCases) of
+      true -> false;
+      false -> true
+      end
+  end.
+
 
 get_int_from_stdin() ->
   {ok, [Data]} = io:fread("","~d"),
