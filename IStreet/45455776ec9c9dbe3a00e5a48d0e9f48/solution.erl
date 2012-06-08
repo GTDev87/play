842e0f2a7961d87f6d7641647a70ba84dcc5348b
Greg <Gregt87@gmail.com>
2012-06-07 23:00:41 -0700

    Modified sol to use almost all list stuff by me

--- IStreet/45455776ec9c9dbe3a00e5a48d0e9f48/solution.erl
+++ IStreet/45455776ec9c9dbe3a00e5a48d0e9f48/solution.erl
@@ -16,36 +16,43 @@ get_case(NumElements) ->
 
 almost_sorted([_,_], true) -> true;
 almost_sorted([_], false) -> true;
-almost_sorted([H|T], true) ->
-  %need to delete Head
-  %io:fwrite("true ~w~w~n", [H, T]),
-  ThirdIndx = lists:nth(2,T),
-  case H > hd(T) of
-    true -> almost_sorted(T, false);
-    false -> case H > ThirdIndx of
-      true -> almost_sorted([H,hd(T)|lists:nthtail(2,T)], false);
-      false -> case hd(T) > ThirdIndx of
-        true -> almost_sorted([H|tl(T)], false);
-        false -> almost_sorted(T, true)
+almost_sorted([A,B,C|T], true) ->
+  case A > B of
+    true -> almost_sorted([B,C|T], false);
+    false -> case A > C of
+      true -> almost_sorted([A,B|T], false);
+      false -> case B > C of
+        true -> almost_sorted([A,C|T], false);
+        false -> almost_sorted([B,C|T], true)
       end
     end
   end;
 
 almost_sorted([H|T], false) ->
-  %io:fwrite("false ~w~w~n", [H,T]),
   case H > hd(T) of
     true -> false;
     false -> almost_sorted(T, false)
   end.
 
+lazy_list_comprehension([],_) -> [];
+lazy_list_comprehension([H|T], F) -> fun() -> [F(H) || lazy_list_comprehension(T,F)] end.
+lazy_list_comprehension([],_,_) -> [];
+lazy_list_comprehension([H|T],A,F) -> fun() -> [F(H,A) | lazy_list_comprehension(T,A,F)] end.
+
+lazy_list_all(_,[]) -> true;
+lazy_list_all(F, LazyList) ->
+  case F(hd(LazyList())) of
+    true -> lazy_list_all(F,tl(LazyList()));
+    false -> false
+  end.
+
 win_test_case(TestCase) ->
   case almost_sorted(TestCase, true) of
     true ->
-      %io:fwrite("sorted bitches ~n"),
       true;
-    false -> 
-      %io:fwrite("not sorted cont ~n"),
-      case lists:all(fun(Element) -> win_test_case(Element) end, [lists:delete(Element, TestCase) || Element <- TestCase]) of
+    false ->
+      LazyDeletionCombinations = lazy_list_comprehension(TestCase, TestCase, fun(Ele, Array) -> Array -- [Ele] end),
+      case lazy_list_all(fun(Element) -> win_test_case(Element) end, LazyDeletionCombinations) of
       true -> false;
       false -> true
     end
