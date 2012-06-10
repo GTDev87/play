6bf69393db8fe10101d19e59b1b37808e0a2b782
Greg <Gregt87@gmail.com>
2012-06-10 14:44:50 -0700

    Add erlang archive attemps

---
+++ IStreet/45455776ec9c9dbe3a00e5a48d0e9f48/archive_attempt/erlang/list_first_attempt.erl
@@ -0,0 +1,52 @@
+-module(solution).
+-export([main/0]).
+-import(lists).
+
+main() ->
+  lists:foreach(fun(Solution) -> io:fwrite("~s~n", [is_alice(Solution)]) end, get_test_cases(get_int_from_stdin())).
+
+map_n_times(_, 0) -> [];
+map_n_times(F, Times) -> [F() | map_n_times(F, Times-1)].
+ 
+get_test_cases(NumCases) ->
+  map_n_times(fun() -> win_test_case(get_case(get_int_from_stdin())) end, NumCases).
+
+get_case(NumElements) ->
+  map_n_times(fun() -> get_int_from_stdin() end, NumElements).
+
+almost_sorted([_,_], true) -> true;
+almost_sorted([_], false) -> true;
+almost_sorted([H|T], true) ->
+  ThirdIndx = lists:nth(2,T),
+  case H > hd(T) of
+    true -> almost_sorted(T, false);
+    false -> case H > ThirdIndx of
+      true -> almost_sorted([H,hd(T)|lists:nthtail(2,T)], false);
+      false -> case hd(T) > ThirdIndx of
+        true -> almost_sorted([H|tl(T)], false);
+        false -> almost_sorted(T, true)
+      end
+    end
+  end;
+almost_sorted([H|T], false) ->
+  case H > hd(T) of
+    true -> false;
+    false -> almost_sorted(T, false)
+  end.
+
+win_test_case(TestCase) ->
+  case almost_sorted(TestCase, true) of
+    true -> true;
+    false -> 
+      case lists:all(fun(Element) -> win_test_case(Element) end, [lists:delete(Element, TestCase) || Element <- TestCase]) of
+      true -> false;
+      false -> true
+    end
+  end.
+
+get_int_from_stdin() ->
+  {ok, [Data]} = io:fread("","~d"),
+  Data.
+
+is_alice(true) -> "Alice";
+is_alice(false) -> "Bob".
---
+++ IStreet/45455776ec9c9dbe3a00e5a48d0e9f48/archive_attempt/erlang/sol_beautiful_erlang.erl
@@ -0,0 +1,35 @@
+-module(solution).
+-export([main/0]).
+-import(lists).
+
+main() -> lists:foreach(fun(Solution) -> io:fwrite("~s~n", [is_alice(Solution)]) end, get_test_cases(get_int_from_stdin())).
+
+map_n_times(_, 0) -> [];
+map_n_times(F, Times) -> [F() | map_n_times(F, Times-1)].
+ 
+get_test_cases(NumCases) -> map_n_times(fun() -> win_test_case(get_case(get_int_from_stdin())) end, NumCases).
+
+get_case(NumElements) -> map_n_times(fun() -> get_int_from_stdin() end, NumElements).
+
+almost_sorted([_,_]) -> true;
+almost_sorted([X,Y,Z|T]) when X > Y -> is_sorted([Y,Z|T]);
+almost_sorted([X,Y,Z|T]) when X > Z -> is_sorted([X,Y|T]);
+almost_sorted([X,Y,Z|T]) when Y > Z -> is_sorted([X,Z|T]);
+almost_sorted([_,Y,Z|T]) -> almost_sorted([Y,Z|T]).
+
+is_sorted([_]) -> true;
+is_sorted([X,Y|_]) when X > Y -> false;
+is_sorted([_,Y|T]) -> is_sorted([Y|T]).
+
+win_test_case(TestCase) ->
+  case almost_sorted(TestCase) of
+  true -> true;
+  false -> not lists:all(fun(Element) -> win_test_case(TestCase -- [Element]) end, TestCase)
+  end.
+
+get_int_from_stdin() ->
+  {ok, [Data]} = io:fread("","~d"),
+  Data.
+
+is_alice(true) -> "Alice";
+is_alice(false) -> "Bob".
